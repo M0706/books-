dcc1a17b330fa3ebe69020d29f46042f4f672ae5
M0706 <43419626+M0706@users.noreply.github.com>
2019-08-31 17:55:37 +0530

    Update bookstorebend.py

--- bookstorebend.py
+++ bookstorebend.py
@@ -1,11 +1,11 @@
 import psycopg2
-
-def connect():
-	 connection = psycopg2.connect(user = "postgres",
-                                  password = "dhruv_03",
+ connection = psycopg2.connect(user = "postgres",
+                                  password = ".......",
                                   host = "127.0.0.1",
                                   port = "8080",
                                   database = "myfirst")
+def connect():
+	
 	 cur = connection.cursor()
 	 cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY,title CHARACTER VARYING , author CHARACTER VARYING , year INTEGER , isbn INTEGER)")
 	 connection.commit()
@@ -13,11 +13,7 @@ def connect():
 
 
 def insert(title,author,year,isbn):
-	connection = psycopg2.connect(user = "postgres",
-                                  password = "dhruv_03",
-                                  host = "127.0.0.1",
-                                  port = "8080",
-                                  database = "myfirst")
+	
 	cur = connection.cursor()
 	cur.execute("INSERT INTO book(title,author,year,isbn)VALUES('%s','%s',%d,%d)"%(title,author,year,isbn))
 	connection.commit()
@@ -25,11 +21,7 @@ def insert(title,author,year,isbn):
 
 
 def view():
-	connection = psycopg2.connect(user = "postgres",
-                                  password = "dhruv_03",
-                                  host = "127.0.0.1",
-                                  port = "8080",
-                                  database = "myfirst")
+	
 	cur = connection.cursor()
 	cur.execute("Select * from book")
 	rows=cur.fetchall()
@@ -37,11 +29,7 @@ def view():
 	return rows
 
 def search(title="",author=""):
-	connection = psycopg2.connect(user = "postgres",
-                                  password = "dhruv_03",
-                                  host = "127.0.0.1",
-                                  port = "8080",
-                                  database = "myfirst")
+	
 	cur = connection.cursor()
 	cur.execute("Select * from book where title='%s' OR author='%s' "%(title,author))
 	rows=cur.fetchall()
@@ -49,22 +37,14 @@ def search(title="",author=""):
 	return rows
 
 def delete(id):
-	connection = psycopg2.connect(user = "postgres",
-                                  password = "dhruv_03",
-                                  host = "127.0.0.1",
-                                  port = "8080",
-                                  database = "myfirst")
+
 	cur = connection.cursor()
 	cur.execute("delete from book where id = %d"%(id))
 	connection.commit()
 	connection.close()
 
 def update(id,title,author,year,isbn):
-	connection = psycopg2.connect(user = "postgres",
-                                  password = "dhruv_03",
-                                  host = "127.0.0.1",
-                                  port = "8080",
-                                  database = "myfirst")
+	
 	cur = connection.cursor()
 	cur.execute("UPDATE book SET title='%s' , author='%s' , year=%d , isbn=%d where id='%d'"%(title,author,year,isbn,id)) 
 	connection.commit()
